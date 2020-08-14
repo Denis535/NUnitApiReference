932c3ee253349513d26267014842847c47745094
Denis535 <dddenisss35@gmail.com>
2020-08-14 21:53:33 +0300

    Fix

--- ApiReference/ApiReference/MarkdownRenderer.cs
+++ ApiReference/ApiReference/MarkdownRenderer.cs
@@ -18,13 +18,16 @@ namespace ApiReference {
         public static void Render(StringBuilder builder, IEnumerable<object> items) {
             foreach (var (item, id) in items.WithId()) {
                 if (item is Project proj) {
-                    builder.AppendFormatLine( "  - [{0}] (#{1}-{2})", proj, proj.ToString().ToLowerInvariant(), id );
+                    var name = proj.ToString().Replace( ":", "" );
+                    builder.AppendFormatLine( "  - [{0}] (#{1}-{2})", name, name.ToLowerInvariant(), id );
                 }
                 if (item is Module module) {
-                    builder.AppendFormatLine( "    - [{0}] (#{1}-{2})", module, module.ToString().ToLowerInvariant(), id );
+                    var name = module.ToString().Replace( ":", "" );
+                    builder.AppendFormatLine( "    - [{0}] (#{1}-{2})", name, name.ToLowerInvariant(), id );
                 }
                 if (item is Namespace @namespace) {
-                    builder.AppendFormatLine( "      - [{0}] (#{1}-{2})", @namespace, @namespace.ToString().ToLowerInvariant(), id );
+                    var name = @namespace.ToString().Replace( ":", "" );
+                    builder.AppendFormatLine( "      - [{0}] (#{1}-{2})", name, name.ToLowerInvariant(), id );
                 }
             }
 

7f49bef16c45ef2eccbeb42ad4ba2c3ce7dd045d
Denis535 <dddenisss35@gmail.com>
2020-08-19 01:00:08 +0300

    NUnitArchitecture

--- NUnitArchitecture.Tests/NUnitArchitecture.Tests/Tests_NUnitProject.cs
+++ NUnitArchitecture.Tests/NUnitArchitecture.Tests/Tests_NUnitProject.cs
@@ -1,7 +1,7 @@
 // This is an independent project of an individual developer. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
-namespace NUnitApiReference {
+namespace NUnitArchitecture.Tests {
     using System;
     using System.Collections.Generic;
     using System.Linq;
